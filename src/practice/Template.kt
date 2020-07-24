b6565371ee56c27c8be76ae1b2de0f7d0ee469d3
corutopi <39874652+corutopi@users.noreply.github.com>
2020-07-24 18:47:17 +0900

    solve 159 A, B, and more. make Template.

---
+++ src/practice/Template.kt
@@ -0,0 +1,14 @@
+package practice
+
+
+fun solve() {
+
+}
+
+fun main() {
+//    val S = readLine()!!
+//    val N = readLine()!!.toInt()
+//    val (N, M) = readLine()!!.split(" ").map { it.toInt() }
+//    val A = readLine()!!.split(" ").map { it.toInt() }
+    solve()
+}
