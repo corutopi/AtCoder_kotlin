c68fb8dfb2014a25d23891da28df4cac54816607
corutopi <39874652+corutopi@users.noreply.github.com>
2020-07-31 01:18:33 +0900

    solve 156 A, B.

---
+++ src/ABC/at156/B.kt
@@ -0,0 +1,12 @@
+package ABC.at156
+
+fun main() {
+    val (n, k) = readLine()!!.split(" ").map { it.toInt() }
+    var x = n
+    var ans = 0
+    while (x > 0) {
+        ans += 1
+        x /= k
+    }
+    print(ans)
+}
