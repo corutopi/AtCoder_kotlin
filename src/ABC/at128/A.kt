db0b57e6e309930337fae4ae6c88506524a294f1
corutopi <39874652+corutopi@users.noreply.github.com>
2020-09-17 23:43:55 +0900

    solve ABC128 A.

---
+++ src/ABC/at128/A.kt
@@ -0,0 +1,13 @@
+package ABC.at128
+
+fun solve(A: Int, P: Int) {
+    println((A * 3 + P) / 2)
+}
+
+fun main() {
+//    val S = readLine()!!
+//    val N = readLine()!!.toInt()
+    val (A, P) = readLine()!!.split(" ").map { it.toInt() }
+//    val A = readLine()!!.split(" ").map { it.toInt() }
+    solve(A, P)
+}
