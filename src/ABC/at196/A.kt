2b57a2db51e06fce37c280fcd7ecdbdb853d41e7
corutopi <39874652+corutopi@users.noreply.github.com>
2021-05-26 00:57:58 +0900

    solve ABC 196 A.

---
+++ src/ABC/at196/A.kt
@@ -0,0 +1,12 @@
+package ABC.at196
+
+
+fun solve(a: Int, b: Int, c: Int, d: Int) {
+    println(b - c)
+}
+
+fun main() {
+    val (a, b) = readLine()!!.split(" ").map { it.toInt() }
+    val (c, d) = readLine()!!.split(" ").map { it.toInt() }
+    solve(a, b, c, d)
+}
