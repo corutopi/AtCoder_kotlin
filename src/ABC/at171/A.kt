46892a5ba369d693af74eeaa5816f9d97d49ae74
corutopi <39874652+corutopi@users.noreply.github.com>
2021-08-01 00:16:33 +0900

    solve abc171 A.

---
+++ src/ABC/at171/A.kt
@@ -0,0 +1,10 @@
+package ABC.at171
+
+fun solve(a: String) {
+    println(if (Regex(pattern = "[A-Z]").containsMatchIn(a)) "A" else "a")
+}
+
+fun main() {
+    val a = readLine()!!
+    solve(a)
+}
