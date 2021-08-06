fc16609e5c051029834783d47fdfd70f79aa31f2
corutopi <39874652+corutopi@users.noreply.github.com>
2021-08-06 23:04:51 +0900

    solve abc109 A.

---
+++ src/ABC/at109/A.kt
@@ -0,0 +1,16 @@
+package ABC.at109
+
+fun solve(a: Int, b: Int) {
+    when (a * b % 2) {
+        0 -> println("No")
+        1 -> println("Yes")
+    }
+}
+
+fun main() {
+//    val s = readLine()!!
+//    val n = readLine()!!.toInt()
+    val (a, b) = readLine()!!.split(" ").map { it.toInt() }
+//    val a = readLine()!!.split(" ").map { it.toInt() }
+    solve(a, b)
+}
