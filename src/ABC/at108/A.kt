8cffe42274cb1af1cd80193ab08e01b258ad79f8
corutopi <39874652+corutopi@users.noreply.github.com>
2021-08-06 23:11:25 +0900

    solve abc108 A.

---
+++ src/ABC/at108/A.kt
@@ -0,0 +1,16 @@
+package ABC.at108
+
+fun solve(k:Int) {
+    when (k % 2) {
+        0 -> println((k / 2) * (k / 2))
+        1 -> println((k / 2) * (k / 2 + 1))
+    }
+}
+
+fun main() {
+//    val s = readLine()!!
+    val k = readLine()!!.toInt()
+//    val (n, m) = readLine()!!.split(" ").map { it.toInt() }
+//    val a = readLine()!!.split(" ").map { it.toInt() }
+    solve(k)
+}
