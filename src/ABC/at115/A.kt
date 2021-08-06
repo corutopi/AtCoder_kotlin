c23131f7f3ff6120a569ec16824c34002b7e1a37
corutopi <39874652+corutopi@users.noreply.github.com>
2021-08-06 16:07:44 +0900

    solve abc115 A.

---
+++ src/ABC/at115/A.kt
@@ -0,0 +1,20 @@
+package ABC.at115
+
+fun solve(d: Int) {
+    var ans = ""
+    when (d) {
+        25 -> ans = "Christmas"
+        24 -> ans = "Christmas Eve"
+        23 -> ans = "Christmas Eve Eve"
+        22 -> ans = "Christmas Eve Eve Eve"
+    }
+    println(ans)
+}
+
+fun main() {
+//    val s = readLine()!!
+    val d = readLine()!!.toInt()
+//    val (n, m) = readLine()!!.split(" ").map { it.toInt() }
+//    val a = readLine()!!.split(" ").map { it.toInt() }
+    solve(d)
+}
