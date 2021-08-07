045907d1e9e5329e2f7c6596c577300478a091ce
corutopi <39874652+corutopi@users.noreply.github.com>
2021-08-08 00:59:17 +0900

    solve abc197 A.

--- src/ABC/at197/A.kt
+++ src/ABC/at197/A.kt
@@ -1,16 +1,13 @@
-package practice
+package ABC.at197
 
-import java.util.*
-
-
-fun solveX() {
-
+fun solveX(s: String) {
+    println(s.substring(1) + s.substring(0, 1))
 }
 
 fun main() {
-//    val s = readLine()!!
+    val s = readLine()!!
 //    val n = readLine()!!.toInt()
 //    val (n, m) = readLine()!!.split(" ").map { it.toInt() }
 //    val a = readLine()!!.split(" ").map { it.toInt() }
-    solveX()
+    solveX(s)
 }
