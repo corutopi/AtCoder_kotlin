bbaec5870d6e47ddd3c1f3138b3114a3d5e6b8e6
corutopi <39874652+corutopi@users.noreply.github.com>
2022-03-25 10:36:02 +0900

    solve abc007 A, abc008 A.

---
+++ src/ABC/at008/A.kt
@@ -0,0 +1,13 @@
+package ABC.at008
+
+fun solveX(s: Int, t: Int) {
+    println(t - s + 1)
+}
+
+fun main() {
+//    val s = readLine()!!
+//    val n = readLine()!!.toInt()
+    val (s, t) = readLine()!!.split(" ").map { it.toInt() }
+//    val a = readLine()!!.split(" ").map { it.toInt() }
+    solveX(s, t)
+}
