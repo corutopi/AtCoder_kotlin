6a57940ab2d7cfe75784926975c8fb63778ca687
corutopi <39874652+corutopi@users.noreply.github.com>
2022-03-25 10:31:30 +0900

    solve abc006 A.

--- src/ABC/at006/A.kt
+++ src/ABC/at006/A.kt
@@ -1,14 +1,13 @@
-package practice
+package ABC.at006
 
-
-fun solveX() {
-
+fun solveX(n: Int) {
+    println(if (n % 3 == 0) { "YES" } else { "NO" })
 }
 
 fun main() {
 //    val s = readLine()!!
-//    val n = readLine()!!.toInt()
+    val n = readLine()!!.toInt()
 //    val (n, m) = readLine()!!.split(" ").map { it.toInt() }
 //    val a = readLine()!!.split(" ").map { it.toInt() }
-    solveX()
+    solveX(n)
 }
