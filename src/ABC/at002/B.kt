f98c6179877e667144704e86a4c2683ca804c36c
corutopi <39874652+corutopi@users.noreply.github.com>
2022-04-01 18:39:58 +0900

    solve abc002 B, abc003 B.

---
+++ src/ABC/at002/B.kt
@@ -0,0 +1,17 @@
+package ABC.at002
+
+fun solveX(w: String) {
+    println(w.replace("a", "")
+            .replace("i", "")
+            .replace("u", "")
+            .replace("e", "")
+            .replace("o", ""))
+}
+
+fun main() {
+    val w = readLine()!!
+//    val n = readLine()!!.toInt()
+//    val (n, m) = readLine()!!.split(" ").map { it.toInt() }
+//    val a = readLine()!!.split(" ").map { it.toInt() }
+    solveX(w)
+}
---
+++ src/ABC/at003/B.kt
@@ -0,0 +1,23 @@
+package ABC.at003
+
+fun solveX(s: String, t: String) {
+    val keyword = "atcoder@"
+    for (i in s.indices) {
+        if (s[i] == t[i]) continue
+        if ((s[i] == '@' || t[i] == '@')
+                && s[i] in keyword
+                && t[i] in keyword) continue
+        println("You will lose")
+        return
+    }
+    println("You can win")
+}
+
+fun main() {
+    val s = readLine()!!
+    val t = readLine()!!
+//    val n = readLine()!!.toInt()
+//    val (n, m) = readLine()!!.split(" ").map { it.toInt() }
+//    val a = readLine()!!.split(" ").map { it.toInt() }
+    solveX(s, t)
+}
