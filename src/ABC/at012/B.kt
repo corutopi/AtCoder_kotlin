89a95232b41db6e90bdbc590c3ba1fbe58e817a8
corutopi <39874652+corutopi@users.noreply.github.com>
2022-05-06 17:32:10 +0900

    solve some task.
    abc005 B.
    abc006 B.
    abc012 A, B.
    abc013 A.

---
+++ src/ABC/at012/B.kt
@@ -0,0 +1,13 @@
+package ABC.at012
+
+fun solveX(n: Int) {
+    val h = n / (60 * 60)
+    val m = n % (60 * 60) / 60
+    val s = n % 60
+    println(String.format("%02d:%02d:%02d", h, m, s))
+}
+
+fun main() {
+    val n = readLine()!!.toInt()
+    solveX(n)
+}
