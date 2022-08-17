b343717e5fcbc3161d9203eebb1557dda97890f0
corutopi <39874652+corutopi@users.noreply.github.com>
2022-08-18 00:00:29 +0900

    solve some task.
    abc019 A, B.
    abc020 A, B.

---
+++ src/ABC/at019/A.kt
@@ -0,0 +1,14 @@
+package ABC.at019
+
+fun solveX(a: List<Int>) {
+    println(a.sorted()[1])
+}
+
+fun main() {
+//    val s = readLine()!!
+//    val n = readLine()!!.toInt()
+//    val (n, m) = readLine()!!.split(" ").map { it.toInt() }
+//    val t = Array(n) { readLine()!! }
+    val a = readLine()!!.split(" ").map { it.toInt() }
+    solveX(a)
+}
---
+++ src/ABC/at019/B.kt
@@ -0,0 +1,21 @@
+package ABC.at019
+
+fun solveX(s: String) {
+    val tmp_s = s + "X"
+    var ans = ""
+    var cnt = 0
+    for (i in 0 until tmp_s.length) {
+        if ((i == 0) || (tmp_s[i] == tmp_s[i - 1])) {
+            cnt += 1
+        } else {
+            ans += tmp_s[i - 1] + cnt.toString()
+            cnt = 1
+        }
+    }
+    println(ans)
+}
+
+fun main() {
+    val s = readLine()!!
+    solveX(s)
+}
